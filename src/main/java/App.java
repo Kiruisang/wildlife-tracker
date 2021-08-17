d872b17b811999d0d8d48b6f746c9fa86d89118e
Kirui sang <brian.kirui@student.moringaschool.com>
2021-08-17 19:11:31 +0300

    initial commit of the project

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.QueryParamsMap;
+import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import static spark.Spark.*;
+import org.apache.log4j.BasicConfigurator;
+
 public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+
+        get("/",((request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("Rangers", Ranger.all());
+            model.put("")
+        })
+    },new HandlebarsTemplateEngine());
+    }
+
+    private static ModelAndView handle(Request request, Response response) {
+        Map<String, Object> model = new HashMap<>();
+        model.put("Rangers", Ranger.all());
+        model.put("template", "templates/index.hbs");
+        return new ModelAndView(model, "index.hbs");
+    }
 }
